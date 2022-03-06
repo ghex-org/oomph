148ffb321d59ed945961b5956df13728ab29ff54
Marcin Krotkiewski <marcin.krotkiewski@gmail.com>
2022-03-06 19:47:19 +0100

    change ucp lib discovery and linking

--- cmake/FindUCP.cmake
+++ cmake/FindUCP.cmake
@@ -3,32 +3,31 @@ pkg_check_modules(PC_UCX QUIET ucx)
 
 find_path(UCP_INCLUDE_DIR ucp/api/ucp.h
     HINTS
+    ${HPCX_UCX_DIR}   ENV HPCX_UCX_DIR
     ${UCX_ROOT}  ENV UCX_ROOT
     ${UCX_DIR}   ENV UCX_DIR
     ${UCP_ROOT}  ENV UCX_ROOT
     ${UCP_DIR}   ENV UCX_DIR
     PATH_SUFFIXES include)
 
-find_library(UCP_LIBRARY HINT ${UCP_DIR} NAMES ucp
+find_path(UCP_LIBRARY_DIR libucp.so
     HINTS
+    ${HPCX_UCX_DIR}   ENV HPCX_UCX_DIR
     ${UCX_ROOT}  ENV UCX_ROOT
     ${UCX_DIR}   ENV UCX_DIR
     ${UCP_ROOT}  ENV UCX_ROOT
     ${UCP_DIR}   ENV UCX_DIR
     PATH_SUFFIXES lib lib64)
 
-set(UCP_LIBRARIES    ${UCP_LIBRARY} CACHE INTERNAL "")
-set(UCP_INCLUDE_DIRS ${UCP_INCLUDE_DIR} CACHE INTERNAL "")
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UCP DEFAULT_MSG UCP_LIBRARY UCP_INCLUDE_DIR)
-
-mark_as_advanced(UCX_ROOT UCP_LIBRARY UCP_INCLUDE_DIR)
+find_package_handle_standard_args(UCP DEFAULT_MSG UCP_LIBRARY_DIR UCP_INCLUDE_DIR)
+mark_as_advanced(UCP_LIBRARY_DIR UCP_INCLUDE_DIR)
 
 if(NOT TARGET UCP::libucp AND UCP_FOUND)
-  add_library(UCP::libucp SHARED IMPORTED)
+  add_library(UCP::libucp INTERFACE IMPORTED)
   set_target_properties(UCP::libucp PROPERTIES
-    IMPORTED_LOCATION ${UCP_LIBRARY}
+    INTERFACE_LINK_LIBRARIES "ucp;ucs;uct"
+    INTERFACE_LINK_DIRECTORIES ${UCP_LIBRARY_DIR}
     INTERFACE_INCLUDE_DIRECTORIES ${UCP_INCLUDE_DIR}
   )
 endif()
--- cmake/oomph_common.cmake
+++ cmake/oomph_common.cmake
@@ -8,24 +8,28 @@ mark_as_advanced(OOMPH_USE_FAST_PIMPL)
 # ---------------------------------------------------------------------
 # compiler and linker flags
 # ---------------------------------------------------------------------
-set(cxx_lang "$<COMPILE_LANGUAGE:CXX>")
+set(cxx_lang_gnu "$<COMPILE_LANG_AND_ID:CXX,GNU>")
 set(cxx_lang_clang "$<COMPILE_LANG_AND_ID:CXX,Clang>")
+set(cxx_lang_intel "$<COMPILE_LANG_AND_ID:CXX,Intel>")
 #set(cuda_lang "$<COMPILE_LANGUAGE:CUDA>")
-set(fortran_lang "$<COMPILE_LANGUAGE:Fortran>")
 set(fortran_lang_gnu "$<COMPILE_LANG_AND_ID:Fortran,GNU>")
+set(fortran_lang_intel "$<COMPILE_LANG_AND_ID:Fortran,Intel>")
+set(fortran_lang_cray "$<COMPILE_LANG_AND_ID:Fortran,Cray>")
 
 function(oomph_target_compile_options target)
     set_target_properties(${target} PROPERTIES INTERFACE_POSITION_INDEPENDENT_CODE ON)
-    #target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic)
+    # target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic)
     target_compile_options(${target} PRIVATE
     # flags for CXX builds
-    $<${cxx_lang}:$<BUILD_INTERFACE:-Wall -Wextra -Wpedantic -Wno-unknown-pragmas -Wno-unused-local-typedef>>
+    $<${cxx_lang_gnu}:$<BUILD_INTERFACE:-Wall -Wextra -Wpedantic -Wno-unknown-pragmas -Wno-unused-local-typedef>>
     $<${cxx_lang_clang}:$<BUILD_INTERFACE:-Wno-c++17-extensions -Wno-unused-lambda-capture>>
+    $<${cxx_lang_intel}:$<BUILD_INTERFACE:-Wall -Wextra -Wno-unknown-pragmas>>
     # flags for CUDA builds
     #$<${cuda_lang}:$<BUILD_INTERFACE:-Xcompiler=-Wall -Wextra -Wno-unknown-pragmas --default-stream per-thread>>
     # flags for Fortran builds
-    $<${fortran_lang}:$<BUILD_INTERFACE:-cpp -fcoarray=single>>
-    $<${fortran_lang_gnu}:$<BUILD_INTERFACE:-ffree-line-length-none>>)
+    $<${fortran_lang_gnu}:$<BUILD_INTERFACE:-cpp -ffree-line-length-none>>
+    $<${fortran_lang_intel}:$<BUILD_INTERFACE:-cpp>>
+    $<${fortran_lang_cray}:$<BUILD_INTERFACE:-eZ>>)
     if (OOMPH_ENABLE_BARRIER)
         target_compile_definitions(${target} PRIVATE OOMPH_ENABLE_BARRIER)
     endif()
