95bee0d49e9ee5810bdac2f2c8c14ac50961b1c4
Marcin Krotkiewski <marcin.krotkiewski@gmail.com>
2022-03-11 12:42:27 +0100

    add OOMPH_FORTRAN_OPENMP to cmake compile bindings with OpenMP if it is set Intel Fortran modules have to be compiled with OpenMP if the modules will ever be used in an OpenMP application

--- cmake/oomph_fortran.cmake
+++ cmake/oomph_fortran.cmake
@@ -2,6 +2,7 @@ set(OOMPH_BUILD_FORTRAN OFF CACHE BOOL "build FORTRAN bindings")
 
 if (OOMPH_BUILD_FORTRAN)
     enable_language(Fortran)
+    set(OOMPH_FORTRAN_OPENMP "ON" CACHE BOOL "Compile Fortran bindings with OpenMP")
     set(OOMPH_FORTRAN_FP "float" CACHE STRING "Floating-point type")
     set_property(CACHE OOMPH_FORTRAN_FP PROPERTY STRINGS "float" "double")
     if(${OOMPH_FORTRAN_FP} STREQUAL "float")
@@ -10,9 +11,13 @@ if (OOMPH_BUILD_FORTRAN)
         set(OOMPH_FORTRAN_FP_KIND 8)
     endif()
 
-    #set(OOMPH_FORTRAN_OPENMP "ON" CACHE BOOL "Compile Fortran bindings with OpenMP")
-
     find_package(MPI REQUIRED)
+
+    # Intel Fortran modules have to be compiled with OpenMP
+    # if the modules will ever be used in an OpenMP application
+    if(OOMPH_FORTRAN_OPENMP)
+      find_package(OpenMP REQUIRED)
+    endif()
 
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/oomph_defs.f90.in
         ${CMAKE_CURRENT_BINARY_DIR}/bindings/fortran/oomph_defs.f90 @ONLY)
