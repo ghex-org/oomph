5f1ad2252a08054c24c943992043b63009300c2b
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2022-12-15 14:05:52 +0100

    gtest

---
+++ cmake/ext.cmake
@@ -0,0 +1,33 @@
+include(git_submodule)
+include(external_project)
+
+if(OOMPH_GIT_SUBMODULE)
+    update_git_submodules()
+endif()
+
+cmake_dependent_option(OOMPH_USE_BUNDLED_HWMALLOC "Use bundled pika lib." ON
+    "OOMPH_USE_BUNDLED_LIBS" OFF)
+if(OOMPH_USE_BUNDLED_HWMALLOC)
+    check_git_submodule(pika ext/hwmalloc)
+    #set(WITH_FUNCTION_X OFF CACHE BOOL "enable X functionality" FORCE)
+    add_subdirectory(ext/hwmalloc)
+else()
+    find_package(HWMALLOC REQUIRED)
+endif()
+
+# Set up google test as an external project
+add_external_cmake_project(
+    NAME googletest
+    PATH ext/googletest
+    INTERFACE_NAME ext-gtest
+    LIBS libgtest.a libgtest_main.a
+    CMAKE_ARGS
+        "-DCMAKE_BUILD_TYPE=release"
+        "-DBUILD_SHARED_LIBS=OFF"
+        "-DBUILD_GMOCK=OFF")
+
+# on some systems we need link explicitly against threads
+if (TARGET ext-gtest)
+    find_package (Threads)
+    target_link_libraries(ext-gtest INTERFACE Threads::Threads)
+endif()
--- cmake/oomph_common.cmake
+++ cmake/oomph_common.cmake
@@ -33,7 +33,8 @@ endfunction()
 function(oomph_shared_lib_options target)
     oomph_target_compile_options(${target})
     oomph_target_link_options(${target})
-    target_link_libraries(${target} PUBLIC HWMALLOC::hwmalloc)
+    #target_link_libraries(${target} PUBLIC HWMALLOC::hwmalloc)
+    target_link_libraries(${target} PUBLIC hwmalloc)
 endfunction()
 
 # ---------------------------------------------------------------------
