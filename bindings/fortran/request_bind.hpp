ff1922de6f170d06fb3874297ae8a42c9acf4b37
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2023-05-11 11:15:25 +0200

    license

--- bindings/fortran/request_bind.hpp
+++ bindings/fortran/request_bind.hpp
@@ -1,3 +1,12 @@
+/*
+ * ghex-org
+ *
+ * Copyright (c) 2014-2023, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 #ifndef INCLUDED_OOMPH_FORTRAN_REQUEST_BIND_HPP
 #define INCLUDED_OOMPH_FORTRAN_REQUEST_BIND_HPP
 
