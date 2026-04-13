e55f1ad2244718cd44b3d9db98bc2020b3ad97c7
Mikael Simberg <mikael.simberg@iki.fi>
2026-04-13 17:07:18 +0200

    Fix testing path

--- .cscs-ci/scripts/ctest-bind-testing-dir.sh
+++ .cscs-ci/scripts/ctest-bind-testing-dir.sh
@@ -4,6 +4,7 @@
 # directory for each process when running ctest within slurm.
 
 set -x
+mkdir -p "/tmp/Testing/Temporary-${SLURM_PROCID}"
 unshare --mount --map-root-user \
     bash -c \
     "mount --bind /tmp/Testing/Temporary-${SLURM_PROCID} $PWD/Testing/Temporary && $@"
