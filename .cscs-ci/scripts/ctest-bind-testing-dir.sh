841b9bfc27a9daaf7b8f0a5e3bd809440f9f06c6
Mikael Simberg <mikael.simberg@iki.fi>
2026-04-13 16:06:28 +0200

    Use separate Testing directory for ctest per process

---
+++ .cscs-ci/scripts/ctest-bind-testing-dir.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+#
+# Helper script to mount a separate directory for the Testing/Temporary
+# directory for each process when running ctest within slurm.
+
+set -x
+unshare --mount --map-root-user \
+    bash -c \
+    "mount --bind /tmp/Testing/Temporary-${SLURM_PROCID} $PWD/Testing/Temporary && exec \"$@\""
