be607979a0ad25949a981bcdf3a5aceb48c86a5d
Mikael Simberg <mikael.simberg@iki.fi>
2026-06-03 10:50:48 +0200

    Add missing file

---
+++ test/nccl_test_helpers.hpp
@@ -0,0 +1,42 @@
+/*
+ * ghex-org
+ *
+ * Copyright (c) 2014-2026, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#pragma once
+
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <string>
+
+#include <oomph/context.hpp>
+
+namespace oomph
+{
+namespace test
+{
+inline bool
+is_nccl_backend()
+{
+    return oomph::context(MPI_COMM_WORLD, false).get_transport_option("name") ==
+           std::string("nccl");
+}
+
+inline void
+handle_nccl_thread_safe_exception(std::runtime_error const& e)
+{
+    if (is_nccl_backend())
+    {
+        EXPECT_EQ(e.what(), std::string("NCCL not supported with thread_safe = true"));
+    }
+    else
+    {
+        throw;
+    }
+}
+} // namespace test
+} // namespace oomph
